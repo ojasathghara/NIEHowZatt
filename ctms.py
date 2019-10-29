613fed6e987c3902d33c9d303fc3be1c7cb70c38
ojaswi825 <ojaswi.athghara98@gmail.com>
2019-10-29 15:11:51 +0530

    Added links skeleton

--- ctms.py
+++ ctms.py
@@ -7,5 +7,33 @@ app = Flask(__name__)
 def home():
     return render_template("home.html",title="Home")
 
+@app.route("/schedule")
+def schedule():
+    return render_template("schedule.html",title="Schedule")
+
+@app.route("/teams")
+def teams():
+    return render_template("teams.html",title="All Teams")
+
+@app.route("/points")
+def points():
+    return render_template("points.html",title="Points")
+
+@app.route("/rules")
+def rules():
+    return render_template("rules.html",title="Rules")
+
+@app.route("/about")
+def about():
+    return render_template("about.html",title="About")
+
+@app.route("/login")
+def login():
+    return render_template("login.html",title="Login admin")
+
+@app.route("/ongoing")
+def ongoing():
+    return render_template("ongoing.html",title="Current Match")
+
 if __name__ == "__main__":
     app.run(debug=True)
